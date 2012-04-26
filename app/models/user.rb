119855f678052ae5c157bdde25def74f87be674b
Joseph Barrozo <JABarrozO@gmail.com>
2012-04-26 10:58:33 +0800

    first try

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :uid, :provider, :fname, :lname, :email, :image
+  def self.create_with_omniauth(auth)
+		create! do |user|
+		  user.provider = auth["provider"]
+		  user.uid = auth["uid"]
+		  user.fname = auth["info"]["first_name"]
+		  user.lname = auth["info"]["last_name"]
+		  user.email = auth["info"]["email"]
+		  user.image = auth["info"]["image"]
+	  end
+	end
 end
